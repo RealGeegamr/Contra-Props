5fd3b62a83cdd870b39ec40774c15ee57abbe798
Geegamr <geegamr@gmail.com>
2025-06-14 08:48:36 -0600

    stuff

--- src/main/java/net/geegamr/contraprops/item/ModCreativeModTabs.java
+++ src/main/java/net/geegamr/contraprops/item/ModCreativeModTabs.java
@@ -14,6 +14,7 @@ public class ModCreativeModTabs {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
         DeferredRegister.create(Registries.CREATIVE_MODE_TAB, contraprops.MODID);
 
+// Items Tab
     public static final RegistryObject<CreativeModeTab> CONTRA_TAB = CREATIVE_MODE_TABS.register("contra_items", 
     () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.METHBOX.get()))
     .title(Component.translatable("creativetab.contra_items"))
@@ -24,14 +25,19 @@ public class ModCreativeModTabs {
         pOutput.accept(ModItems.UNCOOKEDTRAY.get());
         pOutput.accept(ModItems.COOKEDTRAY.get());
         pOutput.accept(ModItems.JOINT.get());
+        pOutput.accept(ModItems.WEED.get());
         pOutput.accept(ModItems.CAUSTICSODA.get());
         pOutput.accept(ModItems.HYDROGENCHLORIDE.get());
         pOutput.accept(ModItems.MURIATICACID.get());
+        pOutput.accept(ModItems.WATER_BOTTLE.get());
+        pOutput.accept(ModItems.ICE_PACK.get());
+        pOutput.accept(ModItems.PAPPER_BAG.get());
     })
     .build());
 
+// Block Tabs
     public static final RegistryObject<CreativeModeTab> CONTRA_BLOCKS = CREATIVE_MODE_TABS.register("contra_blocks", 
-    () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.COOKEDTRAY.get()))
+    () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModBlocks.METH_FRIDGE.get()))
     .title(Component.translatable("creativetab.contra_blocks"))
     .displayItems((pParameters, pOutput) -> {
         pOutput.accept(ModBlocks.METH_FRIDGE.get());
--- src/main/java/net/geegamr/contraprops/item/ModItems.java
+++ src/main/java/net/geegamr/contraprops/item/ModItems.java
@@ -11,40 +11,50 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
         DeferredRegister.create(ForgeRegistries.ITEMS, contraprops.MODID);
 
+// New Items
     public static final RegistryObject<Item> METHBOX = ITEMS.register("meth_box",
-    () -> new Item(new Item.Properties())
-);
+    () -> new Item(new Item.Properties()));
 
     public static final RegistryObject<Item> CHEMICAL = ITEMS.register("chemicals",
-    () -> new Item(new Item.Properties())
-);
+    () -> new Item(new Item.Properties()));
 
     public static final RegistryObject<Item> UNCOOKEDTRAY = ITEMS.register("meth_tray_uncooked",
-    () -> new Item(new Item.Properties())
-);
+    () -> new Item(new Item.Properties()));
 
     public static final RegistryObject<Item> EMPTYTRAY = ITEMS.register("meth_tray_empty",
-    () -> new Item(new Item.Properties())
-);
+    () -> new Item(new Item.Properties()));
 
     public static final RegistryObject<Item> COOKEDTRAY = ITEMS.register("meth_tray_cooked",
-    () -> new Item(new Item.Properties())
-);
+    () -> new Item(new Item.Properties()));
 
     public static final RegistryObject<Item> JOINT = ITEMS.register("joint",
     () -> new Item(new Item.Properties()));
 
         public static final RegistryObject<Item> CAUSTICSODA = ITEMS.register("caustic_soda",
-    () -> new Item(new Item.Properties())
-);
+    () -> new Item(new Item.Properties()));
 
     public static final RegistryObject<Item> HYDROGENCHLORIDE = ITEMS.register("hydrogen_chloride",
-    () -> new Item(new Item.Properties())
-);
+    () -> new Item(new Item.Properties()));
 
     public static final RegistryObject<Item> MURIATICACID = ITEMS.register("muriaticacid",
     () -> new Item(new Item.Properties()));
 
+    public static final RegistryObject<Item> WATER_BOTTLE = ITEMS.register("water_bottle",
+    () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> STEEL = ITEMS.register("steel",
+    () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> ICE_PACK = ITEMS.register("ice_pack",
+    () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> WEED = ITEMS.register("weed",
+    () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> PAPPER_BAG = ITEMS.register("paper_bag",
+    () -> new Item(new Item.Properties()));
+
+//End Of New Items
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
